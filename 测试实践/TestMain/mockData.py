74cb39181918028a32bdd8d152538eafda7e632c
lenovo <1364826576@qq.com>
2019-09-03 12:34:46 +0800

    添加文件参数 目录名修改

--- 测试实践/TestMain/mockData.py
+++
@@ -1,9 +0,0 @@
-from mock import mock
-class Mock:
-    """
-    模拟响应数据
-    """
-    def __init__(self):
-        self.mock_method= mock.Mock(return_value='{"returnState":{"stateCode":0,"errorMsg":"add error msg"}}')
-    def main(self,method,url,data= None,headers = None):
-        return self.mock_method(method,url,data, headers)
