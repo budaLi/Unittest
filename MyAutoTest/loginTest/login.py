9757bccb50f84e3a5a0ed5522aeab73937cccb39
libuda <1364826576@qq.com>
2019-09-17 18:14:02 -0700

    代码重构 oop编程

--- MyAutoTest/loginTest/login.py
+++ MyAutoTest/loginTest/login.py
@@ -5,197 +5,193 @@ import threading
 import datetime
 from queue import Queue
 
-add_user_queue = Queue()  # 添加用户信息队列
-login_header_queue = Queue()  # 登录请求头队列
-check_response_queue = Queue()  # 检测response队列
-del_user_queue = Queue()  # 删除用户队列
-init_user_id = 100000  # 用户起始id
-init_user_pwd = "123456"  # 用户数据默认密码
-functionalRoleList = ""  # 用户默认角色列表
-resourceRoleList = ""  # 用户默认资源列表
-content_lenght = "123"  # 默认添加用户数据字段长度
-thread_num = 200  # 默认开启线程数
-add_user_num = 100  # 默认添加用户数
-login_user_num = 100  # 默认登录用户数
-mock = Mock()
-utils = Utils()
-login_name = "admin"  # 登录用户名
-login_pwd = "123456"  # 登录密码
-login_url = "http://10.0.20.126/VMS2Service.cgi?Cmd=UserLogin"  # 登录接口
-add_user_url = "http://10.0.20.126/VMS2Service.cgi?Cmd=UserAddUser"  # 添加用户接口
-del_user_url = "http://10.0.20.126/VMS2Service.cgi?Cmd=UserDeleteUser"  # 删除用户接口
-add_user_data = '{"accountInfo":{"userID":"%s","username":"%s","password":"123456","functionalRoleList":"",' \
-                '"resourceRoleList":""}} '
 
-add_user_data2 = {"accountInfo":
-                      {"userID": "",
-                       "username": "",
-                       "password": init_user_pwd,
-                       "functionalRoleList": functionalRoleList,
-                       "resourceRoleList": resourceRoleList}
-                  }
+# 20.0.88.xxx  255.255.0.0
+class LoginTest:
+    def __init__(self):
+        self.init_proxy_ip = "20.0.88.%s:80"  # 初始ip
+        self.add_user_queue = Queue()  # 添加用户信息队列
+        self.login_header_queue = Queue()  # 登录请求头队列
+        self.check_response_queue = Queue()  # 检测response队列
+        self.del_user_queue = Queue()  # 删除用户队列
+        self.init_user_id = 100000  # 用户起始id
+        self.init_user_pwd = "123456"  # 用户数据默认密码
+        self.functionalRoleList = ""  # 用户默认角色列表
+        self.resourceRoleList = ""  # 用户默认资源列表
+        self.content_length = "123"  # 默认添加用户数据字段长度
+        self.add_user_num = 700  # 默认添加用户数
+        self.login_user_num = self.add_user_num  # 默认登录用户数
+        self.thread_num = int(self.add_user_num / 50)  # 默认开启线程数
+        self.user_name_prefix = "li"
+        self.mock = Mock()
+        self.utils = Utils()
+        self.login_name = "admin"  # 登录用户名
+        self.login_pwd = "123456"  # 登录密码
+        self.login_url = "http://10.0.20.126/VMS2Service.cgi?Cmd=UserLogin"  # 登录接口
+        self.add_user_url = "http://10.0.20.126/VMS2Service.cgi?Cmd=UserAddUser"  # 添加用户接口
+        self.del_user_url = "http://10.0.20.126/VMS2Service.cgi?Cmd=UserDeleteUser"  # 删除用户接口
+        self.add_user_data = '{"accountInfo":{"userID":"%s","username":"%s","password":"123456",' \
+                             '"functionalRoleList":"",' \
+                             '"resourceRoleList":""}} '
 
-login_headers = utils.encapsulate_headers(login_name, login_pwd)
+        self.add_user_data2 = {"accountInfo":
+                                   {"userID": "",
+                                    "username": "",
+                                    "password": self.init_user_pwd,
+                                    "functionalRoleList": self.functionalRoleList,
+                                    "resourceRoleList": self.resourceRoleList}
+                               }
 
-add_user_header = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate',
-    'Auth-Token': "",
-    'Accept-Language': 'zh-CN',
-    'Connection': 'keep-alive',
-    'Content-Length': "",
-    'Content-Type': 'application/json',
-    'Host': '10.0.20.126',
-    'Referer': 'http://10.0.20.126/',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
-}
+        self.login_headers = self.utils.encapsulate_headers(self.login_name, self.login_pwd)
 
-del_user_header = add_user_header  # 删除用户请求头
+        self.init_add_user_header = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate',
+            'Auth-Token': "",
+            'Accept-Language': 'zh-CN',
+            'Connection': 'keep-alive',
+            'Content-Length': "",
+            'Content-Type': 'application/json',
+            'Host': '10.0.20.126',
+            'Referer': 'http://10.0.20.126/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
+        }
+        self.add_user_header = self.get_add_user_headers()
+        self.del_user_header = self.add_user_header  # 删除用户请求头
 
-response_headers = requests.post(url=login_url, headers=login_headers).headers
-# response_headers = mock.main(method="post", url=login_url, headers=login_headers)
+    def get_add_user_headers(self):
+        response_headers = requests.post(url=self.login_url, headers=self.login_headers).headers
+        # response_headers = mock.main(method="post", url=login_url, headers=login_headers)
+        self.init_add_user_header['Auth-Token'] = response_headers["auth-token"]  # 登录获取authtoken
+        return self.init_add_user_header
 
-print(response_headers)
+    def proxy_pool(self):
+        """
+        ip代理池
+        :return:
+        """
+        pass
 
-add_user_header['Auth-Token'] = response_headers["auth-token"]  # 登录获取authtoken
+    def check_res(self):
+        """
+        状态检测
+        :return:
+        """
+        while not self.check_response_queue.empty():
+            response = self.check_response_queue.get()
+            if response['returnState']['stateCode'] == 0:
+                print("success")
+            else:
+                print("response error", response['returnState']['stateCode'])
 
+    def generate_user_info(self, user_id, max_num=100):
+        """
+        生成用户信息
+        :param user_id: 起始用户id
+        :param max_num: 生成用户个数
+        :return:
+        """
+        for i in range(max_num):
+            user_info = self.add_user_data % (user_id + i, self.user_name_prefix + str(user_id + i))
+            # add_user_data2["accountInfo"]["userID"] = str(user_id + i)
+            # add_user_data2["accountInfo"]["username"] = "li" + str(user_id + i)
+            # user_info = add_user_data2
+            self.add_user_queue.put(user_info)
+            self.login_header_queue.put(self.utils.encapsulate_headers(name=str(user_id + i), password=str(user_id + i)))
+            self.del_user_queue.put(user_id + i)
 
-def check_res():
-    while not check_response_queue.empty():
-        response = check_response_queue.get()
-        if response['returnState']['stateCode'] == 0:
-            print("success")
-        else:
-            print("response error", response['returnState']['stateCode'])
+    def add_user(self):
+        """
+        添加用户
+        :return:
+        """
+        while not self.add_user_queue.empty():
+            user_data = self.add_user_queue.get(timeout=3)
+            response = requests.post(url=self.add_user_url, data=user_data, headers=self.add_user_header)
+            self.check_response_queue.put(response.json())
 
+    def user_login(self):
+        """
+        用户登录
+        :return:
+        """
+        while not self.login_header_queue.empty():
+            headers = self.login_header_queue.get()
+            # response = mock.main(method="post", url=login_url, headers=headers)
+            response = requests.post(url=self.login_url, headers=headers)
+            self.check_response_queue.put(response.json())
 
-def generate_user_info(user_id, max_num=100):
-    """
-    生成用户信息
-    :param user_id: 起始用户id
-    :param max_num: 生成用户个数
-    :return:
-    """
-    global add_user_queue, login_header_queue
-    for i in range(max_num):
-        user_info = add_user_data % (user_id + i, "li" + str(user_id + i))
+    def del_user(self):
+        """
+        删除用户
+        :return:
+        """
+        while not self.del_user_queue.empty():
+            user_id = self.del_user_queue.get()
+            response = requests.post(url=self.del_user_url, data='{"userID": "%s"}' % str(user_id), headers=self.add_user_header)
+            self.check_response_queue.put(response.json())
 
-        # add_user_data2["accountInfo"]["userID"] = str(user_id + i)
-        # add_user_data2["accountInfo"]["username"] = "li" + str(user_id + i)
-        # user_info = add_user_data2
+    def main(self):
+        key = input("是否添加用户 y/n")
+        # 添加用户
+        if key == "y":
+            print("添加用户中")
+            self.generate_user_info(self.init_user_id, self.add_user_num)
+            add_user_thread_list = []
+            for i in range(self.thread_num):
+                add_user_thread = threading.Thread(target=self.add_user, args=())
+                add_user_thread_list.append(add_user_thread)
 
-        add_user_queue.put(user_info)
-        login_header_queue.put(utils.encapsulate_headers(name=str(user_id + i), password=str(user_id + i)))
-        del_user_queue.put(user_id + i)
+            for one in add_user_thread_list:
+                one.start()
+                one.join()
 
+            print("添加完成")
 
-def add_user():
-    global add_user_queue
-    while not add_user_queue.empty():
-        user_data = add_user_queue.get(timeout=3)
-        response = requests.post(url=add_user_url, data=user_data, headers=add_user_header)
-        check_response_queue.put(response.json())
+            key = input("是否进行登录测试 y/n")
+            if key == "y":
+                # 登录测试
+                print("开始登录测试")
+                user_login_thread_list = []
+                start = datetime.datetime.now()
+                for i in range(self.thread_num):
+                    user_login_thread = threading.Thread(target=self.user_login, args=())
+                    user_login_thread_list.append(user_login_thread)
 
+                for one in user_login_thread_list:
+                    one.start()
+                    one.join()
 
-def user_login():
-    global login_header_queue
-    while not login_header_queue.empty():
-        headers = login_header_queue.get()
-        # response = mock.main(method="post", url=login_url, headers=headers)
-        response = requests.post(url=login_url, headers=login_headers)
-        check_response_queue.put(response.json())
+                end = datetime.datetime.now()
+                print("time", end - start)
 
+                print("登录测试完成")
+            else:
+                key = input("是否进行删除测试 y/n")
+                if key == "y":
+                    # 删除用户
+                    print("开始删除测试")
+                    user_del_thread_list = []
+                    for i in range(self.thread_num):
+                        user_del_thread = threading.Thread(target=self.del_user, args=())
+                        user_del_thread_list.append(user_del_thread)
 
-def del_user():
-    global del_user_queue
-    while not del_user_queue.empty():
-        user_id = del_user_queue.get()
-        response = requests.post(url=del_user_url, data='{"userID": "%s"}' % str(user_id), headers=add_user_header)
-        check_response_queue.put(response.json())
+                    for one in user_del_thread_list:
+                        one.start()
+                        one.join()
+                    print("删除测试完成")
+
+                    # 状态检测
+                    check_res_thread_list = []
+                    for i in range(self.thread_num):
+                        check_res_thread = threading.Thread(target=self.check_res, args=())
+                        check_res_thread_list.append(check_res_thread)
+
+                    for one in check_res_thread_list:
+                        one.start()
+                        one.join()
 
 
 if __name__ == "__main__":
-
-    # 添加用户
-    print("添加用户中")
-    generate_user_info(init_user_id, add_user_num)
-    add_user_thread_list = []
-    for i in range(thread_num):
-        add_user_thread = threading.Thread(target=add_user, args=())
-        add_user_thread_list.append(add_user_thread)
-
-    for one in add_user_thread_list:
-        one.start()
-
-    for one in add_user_thread_list:
-        one.join()
-
-    check_res_thread_list = []
-    for i in range(thread_num):
-        check_res_thread = threading.Thread(target=check_res, args=())
-        check_res_thread_list.append(check_res_thread)
-
-    for one in check_res_thread_list:
-        one.start()
-
-    for one in check_res_thread_list:
-        one.join()
-
-    print("添加完成")
-
-    # 登录测试
-
-    print("开始登录测试")
-    user_login_thread_list = []
-    start = datetime.datetime.now()
-    for i in range(thread_num):
-        user_login_thread = threading.Thread(target=user_login, args=())
-        user_login_thread_list.append(user_login_thread)
-
-    for one in user_login_thread_list:
-        one.start()
-
-    for one in user_login_thread_list:
-        one.join()
-
-    check_res_thread_list = []
-    for i in range(thread_num):
-        check_res_thread = threading.Thread(target=check_res, args=())
-        check_res_thread_list.append(check_res_thread)
-
-    for one in check_res_thread_list:
-        one.start()
-
-    for one in check_res_thread_list:
-        one.join()
-
-    end = datetime.datetime.now()
-    print("time", end - start)
-
-    # 删除用户
-
-    print("开始删除测试")
-    user_del_thread_list = []
-    for i in range(thread_num):
-        user_del_thread = threading.Thread(target=del_user, args=())
-        user_del_thread_list.append(user_del_thread)
-
-    for one in user_del_thread_list:
-        one.start()
-
-    for one in user_del_thread_list:
-        one.join()
-
-    check_res_thread_list = []
-    for i in range(thread_num):
-        check_res_thread = threading.Thread(target=check_res, args=())
-        check_res_thread_list.append(check_res_thread)
-
-    for one in check_res_thread_list:
-        one.start()
-
-    for one in check_res_thread_list:
-        one.join()
-
-    end = datetime.datetime.now()
-    print("time", end - start)
+    logintest = LoginTest()
+    logintest.main()
